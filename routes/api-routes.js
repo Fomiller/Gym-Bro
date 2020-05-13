888f21695e17ed5bc4c27209419cf5c72bac46f1
Forrest <Forrestmillerj@gmail.com>
2020-05-13 12:34:14 -0500

    added express.Router()

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,5 +1,17 @@
-module.exports = function(app) {
-  app.get("/api/", (req, res) => {
-    res.send("hello world");
-  });
-};
+const db = require('../models');
+const express = require('express');
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+router.get("/api/users", (req, res) => {
+  res.send("all users");
+});
+
+router.post("/api/signup", (req, res) => {
+  res.send("user signup");
+});
+
+module.exports = router;
