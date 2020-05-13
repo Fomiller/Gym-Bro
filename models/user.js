ae8db2fb660b59b7eaed84f73fa2d949a182e9ef
Forrest <Forrestmillerj@gmail.com>
2020-05-13 15:31:05 -0500

    passport is creating users and hashing the password

--- models/user.js
+++ models/user.js
@@ -1,16 +1,25 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const passportLocalMongoose = require('passport-local-mongoose');
 
 const UserSchema = new Schema({
   email:{
     type: String,
-    require: "Email is required"
+    unique: true,
+    required: (true, 'email is Required')
   },
   password:{
     type: String,
-    require: "Password is required"
+    required: (true, 'Password is Required')
+  },
+  username: {
+    type: String,
+    unique:true,
+    required: (true, 'Username is Required'),
   }
 }, { toJSON: {virtuals: true}});
+
+UserSchema.plugin(passportLocalMongoose, { usernameField: 'email' });
 const User = mongoose.model("User", UserSchema);
 
 module.exports = User;
