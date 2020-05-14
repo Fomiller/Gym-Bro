42ce18eab3426685c1da95e6ec89533bdd9cb424
Forrest <Forrestmillerj@gmail.com>
2020-05-14 17:49:59 -0500

    added logout button when user is logged in.

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import Nav from '../components/nav';
 import { useGlobalContext } from '../utils/globalContext';
+import { logout } from '../utils/API';
 
 export default function HomePage() {
   const [state, dispatch] = useGlobalContext();
+
+  const handleLogout = (e) => {
+    e.preventDefault()
+    logout()
+    dispatch({ type: 'LOGOUT' });
+  }
 
   if(state.user) {
     return (
@@ -11,6 +18,7 @@ export default function HomePage() {
         <Nav/>
         <h1>HomePage</h1>
         <h2>Welcome {state.user.username}!</h2>
+        <button onClick={handleLogout}>LOGOUT</button>
       </div>
     );
   } else {
