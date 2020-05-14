56cf3027b880ad28e22a505dcc420118efec8b35
Forrest <Forrestmillerj@gmail.com>
2020-05-14 12:58:57 -0500

    added pages for login, signup, and home

---
+++ client/src/pages/signup.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import SignupForm from '../components/signup';
+import Nav from '../components/nav';
+
+export default function SignupPage() {
+  return (
+   <div>
+     <Nav/>
+     <SignupForm/>
+   </div>
+  );
+};
