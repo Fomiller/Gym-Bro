4988e83c235ebdd58bd3f283fda089f0503086c0
Forrest <Forrestmillerj@gmail.com>
2020-05-14 12:59:26 -0500

    created components for login, signup, and nav

---
+++ client/src/components/signup/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default function SignupForm() {
+  return (
+    <div>
+      <form>
+        <input
+        placeholder="email"
+        />
+        <input
+        placeholder="username"
+        />
+        <input
+        placeholder="password"
+        />
+        <button>Signup</button>
+      </form>
+    </div>
+  )
+}
