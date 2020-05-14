e5667914cc224af8c4b6c21386d826800c638706
Forrest <Forrestmillerj@gmail.com>
2020-05-14 15:09:35 -0500

    added conditional rendering

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import Nav from '../components/nav';
+import { useGlobalContext } from '../utils/globalContext';
 
 export default function HomePage() {
-  return (
-    <div>
-      <Nav/>
-      <h1>HOME PAGE</h1>
-    </div>
-  );
+  const [state, dispatch] = useGlobalContext();
+
+  if(state.user) {
+    return (
+      <div>
+        <Nav/>
+        <h1>HomePage</h1>
+        <h2>Welcome {state.user.username}!</h2>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <Nav/>
+        <h1>HomePage</h1>
+      </div>
+    )
+  }
 };
