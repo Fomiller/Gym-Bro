9e76d32f12c5271b38787a526f35b5e85158d1ae
Forrest <Forrestmillerj@gmail.com>
2020-05-20 15:54:04 -0500

    added material ui to login form

--- client/src/components/login/index.js
+++ client/src/components/login/index.js
@@ -2,14 +2,35 @@ import React, { useRef } from 'react';
 import { useGlobalContext } from '../../utils/globalContext';
 import { login } from '../../utils/API';
 import { Redirect } from 'react-router-dom';
+// Material-ui imports
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Paper from '@material-ui/core/Paper';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
 
 export default function LoginForm() {
   const [state, dispatch] = useGlobalContext();
   const emailRef = useRef(null);
   const passwordRef = useRef(null);
+  const classes= useStyles();
 
+  // handler functions
   const handleSubmit = async (e) => {
     e.preventDefault();
+
     // send data to server to request login
     const user = await login({
       email: emailRef.current.value,
@@ -24,27 +45,41 @@ export default function LoginForm() {
     }
     // change global state 
     dispatch({ type: "SET_USER", payload: user });
+    dispatch({ type: "LOGIN", payload:true });
   };
 
-  if (state.user) {
+  if (state.loggedIn) {
     return <Redirect to='/home'/>
   } else {
     return (
-      <div>
-        <form>
-        <input
-          ref={emailRef}
-          placeholder="email"
-          />
-          <input
-          ref={passwordRef}
-          placeholder="password"
-          type='password'
-          />
-          {/* <button onClick={handleSubmit}>Login</button> */}
-          <button onClick={handleSubmit}>Login</button>
-        </form>
-      </div>
+      <Container>
+        <Paper className={classes.paper} elevation={3}>
+          <form>
+          <Grid container justify='center' spacing={2}>
+            <Grid item xs={12}>
+              <TextField
+              inputRef={emailRef} 
+              id="standard-basic" 
+              label="Email"
+              fullWidth={true}
+              />
+              </Grid>
+            <Grid item xs={12}>
+              <TextField
+              inputRef={passwordRef} 
+              id="standard-basic" 
+              label="password"
+              fullWidth={true}
+              type='password'
+              />
+            </Grid>
+            <Grid item>
+              <Button color='primary' variant='outlined' onClick={handleSubmit}>Login</Button>
+            </Grid>
+          </Grid>
+          </form>
+        </Paper>
+      </Container>
     )
   }
 }
