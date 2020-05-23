08ee1020663d4e11e4c87cfbcebe6a78f40c0276
Forrest <Forrestmillerj@gmail.com>
2020-05-23 16:59:58 -0500

    updated User model to include an array of exercises in the model. Updated exercise model to include userId

--- models/exercise.js
+++ models/exercise.js
@@ -2,6 +2,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const ExerciseSchema = new Schema({
+  userId: {
+    type: String,
+    required: (true, 'UserId is Required')
+  },
   name: {
     type: String,
     required:(true, 'Exercise requires name')
