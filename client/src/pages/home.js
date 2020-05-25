a3a1f1ddb233e84e5cf72582ca9a0ab65872245d
Forrest <Forrestmillerj@gmail.com>
2020-05-25 15:51:38 -0500

    removed logout button

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -10,12 +10,6 @@ import Box from '@material-ui/core/Box';
 export default function HomePage() {
   const [state, dispatch] = useGlobalContext();
 
-  const handleLogout = (e) => {
-    e.preventDefault()
-    logout()
-    dispatch({ type: 'LOGOUT', payload: false });
-  }
-
   if(state.loggedIn) {
     return (
       <Box>
@@ -24,7 +18,6 @@ export default function HomePage() {
         <div>
           <h1>HomePage</h1>
           <h2>Welcome {state.user.username}!</h2>
-          <Button color='primary' variant='contained' onClick={handleLogout}>Logout</Button>
         </div>
       </Container>
       </Box>
